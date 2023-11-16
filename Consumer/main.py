05ef493fd5c711abbd21fcd9324fd0e0ad771704
Benjamin Flock <bcflock@gmail.com>
2023-11-16 13:02:05 -0500

    consoom

--- Consumer/main.py
+++ Consumer/main.py
@@ -1,6 +1,6 @@
 from kafka import KafkaConsumer
 import requests as rq
-
+import threading
 SERVER  = 'localhost'
 PORT    = 9092
 EVENT_TOPIC = "events"
@@ -8,18 +8,19 @@ PARTICIPANT_TOPIC = "participants"
 GATEWAY = "http://ec2-54-145-190-43.compute-1.amazonaws.com:3000/api"
 EVENT_ENDPOINT = "event"
 PARTICIPANT_ENDPOINT = "participant"
-
+consumer = KafkaConsumer()
 def events():
-    consumer = KafkaConsumer(EVENT_TOPIC, request_timeout_ms=30000)
+    print("events")
+    consumer = KafkaConsumer(EVENT_TOPIC)
     for msg in consumer:
         rsp = rq.post(f'{GATEWAY}/{EVENT_ENDPOINT}', data=msg.value)
 def participants():
-    consumer = KafkaConsumer(PARTICIPANT_TOPIC, request_timeout_ms=30000)
+    print("participants")
+    consumer = KafkaConsumer(PARTICIPANT_TOPIC)
     for msg in consumer:
-        rsp = rq.port(f'{GATEWAY}/{PARTICIPANT_ENDPOINT}')
+        rsp = rq.post(f'{GATEWAY}/{PARTICIPANT_ENDPOINT}', data=msg.value)
 
 
 if __name__ == "__main__":
-    while(True):
-        events()
-        participants()
+    threading.Thread(target=events).start()
+    threading.Thread(target=participants).start()

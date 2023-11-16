1914b353096e86eba6fa86a33b31b0a60dddd533
Andrew Megeath <avm33@case.edu>
2023-11-15 22:24:08 -0500

    added consumer outline

---
+++ Consumer/src/main/java/consumer/Main.java
@@ -0,0 +1,30 @@
+package consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Properties;
+
+public class Main {
+
+    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:9092";
+    private static final String GROUP_ID = "GroupOneEvent";
+
+    public static void main(String[] args){
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(properties);
+
+        Thread mainThread = Thread.currentThread();
+        Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
+
+        
+    }
+
+}
